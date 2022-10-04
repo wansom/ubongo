caf730105980fb9dac732b2c58d43d534158fe23
wansom <ochiengwarren10@gmail.com>
2022-10-04 11:39:57 +0300

    ft:api routes

---
+++ api/routes/video-routes.js
@@ -0,0 +1,24 @@
+const express =require("express")
+const router = express.Router()
+
+// get video
+router.get('get',(req,res,next)=>{
+
+})
+
+//create video
+router.post("/create",(req,res,next)=>{
+
+})
+
+// update video
+router.patch("/update",(req,res,next)=>{
+
+})
+
+//delete video
+router.delete("delete",(req,res,next)=>{
+    
+})
+
+module.exports =router
